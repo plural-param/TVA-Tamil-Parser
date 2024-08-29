e5440cfc7c21a687f0706c13f78417ef2ed5c5bb
sushvin-research <sushvin.marimuthu@research.iiit.ac.in>
2024-08-29 15:20:46 +0530

    Deployment updated.

--- deploy.sh
+++ deploy.sh
@@ -13,14 +13,14 @@ fi
 npm run build
 
 if [ -d "/var/www/html/TamilParserAssetStaticPrefix" ]; then
-    echo "<password>" | sudo -S rm -rf /var/www/html/TamilParserAssetStaticPrefix/*
+    sudo -S rm -rf /var/www/html/TamilParserAssetStaticPrefix/*
 else
-  echo "<password>" | sudo -S mkdir /var/www/html/TamilParserAssetStaticPrefix
+    sudo -S mkdir /var/www/html/TamilParserAssetStaticPrefix
 fi
 
-echo "<password>" | sudo -S mkdir /var/www/html/TamilParserAssetStaticPrefix/_next
+sudo -S mkdir /var/www/html/TamilParserAssetStaticPrefix/_next
 
-echo "<password>" | sudo -S cp -R .next/* /var/www/html/TamilParserAssetStaticPrefix/_next
-echo "<password>" | sudo -S cp -R public/* /var/www/html/TamilParserAssetStaticPrefix/
+sudo -S cp -R .next/* /var/www/html/TamilParserAssetStaticPrefix/_next
+sudo -S cp -R public/* /var/www/html/TamilParserAssetStaticPrefix/
 
 pm2 start "npm run start" --name TamilParser
